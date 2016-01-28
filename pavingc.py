18eb26ec01495deeb060f3165af96aa121bd2dcf
Victor Butyrin <vbutyrin@gmail.com>
2016-01-28 15:39:52 +0600

    Создан файл readme.txt

--- pavingc.py
+++ pavingc.py
@@ -1,18 +1,7 @@
-'''
-Подложка острова Arma: Cold War Assault площадь 9.92 кв. км
-Автор Виктор Бутырин (publius)
-Версия 1.5
-Привязка к масштабу Cold War Assault:
-	1.0000 blender == 100 м CWA
-	0.5000 blender ==  50 м CWA
-	0.1000 blender ==  10 м CWA
-	0.0100 blender ==   1 м CWA
-'''
-
 bl_info = {
     "name": "Paving 64",
     "author": "Victor Butyrin (publius)",
-    "version": (1, 5),
+    "version": (1, 5, 1),
     "blender": (2, 69, 0),
     "location": "View3D > Add > Mesh",
     "description": "Create Cold War Assault 9.92 sq.km terrain",
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Подложка острова Arma: Cold War Assault площадь 9.92 кв. км
+Автор Виктор Бутырин (publius)
+Версия 1.5
+Привязка к масштабу Cold War Assault:
+	1.0000 blender == 100 м CWA
+	0.5000 blender ==  50 м CWA
+	0.1000 blender ==  10 м CWA
+	0.0100 blender ==   1 м CWA
